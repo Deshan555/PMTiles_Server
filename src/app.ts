796f55f4fc21536e419bc551d493f6e7f79d28bc
deshanj-eimsky <deshan.jayashanka@eimsky.com>
2026-05-28 21:54:43 +0530

    feat: implement express application setup with middleware and routes

---
+++ src/app.ts
@@ -0,0 +1,27 @@
+import cors from "cors";
+import express from "express";
+import { env } from "./config/env";
+import { requestMetrics } from "./middleware/requestMetrics";
+import { requestLogger } from "./middleware/requestLogger";
+import { createSystemRoutes } from "./routes/systemRoutes";
+import { createTileRoutes } from "./routes/tileRoutes";
+import type { PmtilesStore } from "./services/pmtilesStore";
+
+export function createApp(store: PmtilesStore) {
+  const app = express();
+
+  app.disable("x-powered-by");
+  app.set("trust proxy", true);
+
+  app.use(cors({
+    origin: env.corsOrigin,
+    exposedHeaders: ["Content-Length", "Content-Range", "Accept-Ranges", "ETag", "Last-Modified"]
+  }));
+
+  app.use(requestLogger);
+  app.use(requestMetrics);
+  app.use(createSystemRoutes());
+  app.use(createTileRoutes(store));
+
+  return app;
+}
